1c32a699a41f25b08deb128466775c5fcf4f79cf
Sanka Bhanu <bhanusanka3@gmail.com>
2025-07-12 21:07:14 +0530

    instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -8,8 +8,8 @@ DOMAIN_NAME="sankadevops.site"
 
 
 #for instance in ${INSTANCES[@]}{
-#for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
+#for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-05a54a9e7c8e7ea30 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]

3247badd9d409ed1cc66a02f2fd74d4f79aaa5d6
Sanka Bhanu <bhanusanka3@gmail.com>
2025-07-12 20:56:04 +0530

    instance creation

---
+++ roboshop.sh
@@ -0,0 +1,28 @@
+#!/bin/Bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-05a54a9e7c8e7ea30"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z057086315H43ZPXTV7M1"
+DOMAIN_NAME="sankadevops.site"
+
+
+for instance in ${INSTANCES[@]}{
+do 
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
+
+
+done
+}

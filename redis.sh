59e9dab728a35d6602f70e6556abedd26e16935e
Sanka Bhanu <bhanusanka3@gmail.com>
2025-07-13 15:59:10 +0530

    user

--- redis.sh
+++ redis.sh
@@ -35,10 +35,10 @@ VALIDATE(){
 }
 
 dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disabling default redis"
+VALIDATE $? "Disabling default redis version"
 
 dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling redis 7"
+VALIDATE $? "Enabling redis:7"
 
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "installing redis"

0e057aa440064429e12e93d909830500a5726e38
Sanka Bhanu <bhanusanka3@gmail.com>
2025-07-13 23:47:34 +0530

    HI

--- shipping.sh
+++ shipping.sh
@@ -1,82 +1,17 @@
 #!/bin/Bash
-START_TIME=$(date +%s)
-USERID=$(id -u)
 
-R="\e[31m"
-G="\e[32m"
-y="\e[33m"
-N="\e[0m"
+source ./common.sh
+app_name=shipping
+check_root
 
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_PATH=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" |tee -a $LOG_FILE
-
-#check the user have root access or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R error: please run with root access $N " | tee -a $LOG_FILE
-    exit 1
-else
-    echo -e "$G running with root access $N" | tee -a $LOG_FILE
-fi
 
 echo "please enter root password to setup"
 read -s MYSQL_ROOT_PASSWORD
 
-#validate function takes input as exit status, and what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+app_setup
+maven_setup
 
-dnf install maven -y &>>$LOG_FILE
-VALIDATE $? "installing maven and java"
-
-id roboshop &>>$LOG_FILE
-if [ $? -ne 0 ]
-then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "creating roboshop system user"
-else
-    echo -e "system user roboshop already created .....$y skipping   $N"
-fi
-
-mkdir -p /app 
-VALIDATE $? "creating app directory"
-
-curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading shipping"
-
-rm -rf /app/*
-cd /app 
-unzip /tmp/shipping.zip &>>$LOG_FILE
-VALIDATE $? "unzipping shipping"
-
-mvn clean package &>>$LOG_FILE
-VALIDATE $? "packaging the shipping application"
-
-mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
-VALIDATE $? "moving and renaming jar file"
-
-cp $SCRIPT_PATH/shipping.service /etc/systemd/system/shipping.service
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Daemon-reload"
-
-systemctl enable shipping &>>$LOG_FILE
-VALIDATE $? "Enabling Shipping"
-
-systemctl start shipping &>>$LOG_FILE
-VALIDATE $? "Starting shipping"
+systemd_setup
 
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "install mysql"
@@ -95,7 +30,4 @@ fi
 systemctl restart shipping
 VALIDATE $? "Restart shipping"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+print_time

4acf8030d296bba5e6f6889fb96d98948d601660
Sanka Bhanu <bhanusanka3@gmail.com>
2025-07-13 15:32:48 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -1,5 +1,5 @@
 #!/bin/Bash
-
+START_TIME=$(date +%s)
 USERID=$(id -u)
 
 R="\e[31m"
@@ -52,3 +52,7 @@ VALIDATE $? "Enabling Redis"
 systemctl start redis  &>>$LOG_FILE
 VALIDATE $? "Started Redis"
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE

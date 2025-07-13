60b50919a3bc15309059c7ee508e499f1fecc212
Sanka Bhanu <bhanusanka3@gmail.com>
2025-07-13 11:36:21 +0530

    Catalogue

---
+++ catalogue.sh
@@ -0,0 +1,75 @@
+#!/bin/Bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_PATH=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" |tee -a $LOG_FILE
+
+#check the user have root access or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R error: please run with root access $N " &>>$LOG_FILE
+    exit 1
+else
+    echo "running with root access" &>>$LOG_FILE
+fi
+
+#validate function takes input as exit status, and what command they tried to install
+VALIDATE() {
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "Installing $2 is ...$G succcess $N" |tee -a $LOG_FILE
+    else    
+        echo -e "installing $2 is ...$R failure $N" |tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enabling nodejs"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "creating roboshop system user"
+
+mkdir /app 
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading catalogue"
+
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzipping catalogue"
+
+npm install 
+VALIDATE $? "installing dependencies"
+
+cp $SCRIPT_PATH/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue &>>$LOG_FILE
+systemctl start catalogue
+VALIDATE $? "Starting catalogue"
+
+cp $SCRIPT_PATH/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb client"
+
+mongosh --host mongodb.sankadevops.site </app/db/master-data.js &>>$LOG_FILE

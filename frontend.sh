41ca2c83e738bd19a0e6cad69a34e38141799867
Sanka Bhanu <bhanusanka3@gmail.com>
2025-07-13 23:02:58 +0530

    HI

--- frontend.sh
+++ frontend.sh
@@ -1,38 +1,7 @@
 #!/Bin/bash
 
-USERID=$(id -u)
-
-R="\e[31m"
-G="\e[32m"
-y="\e[33m"
-N="\e[0m"
-
-LOGS_FOLDER="/var/log/shell-script-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_PATH=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" |tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R error: please run with root access $N " &>>$LOG_FILE
-    exit 1
-else
-    echo "running with root access" &>>$LOG_FILE
-fi
-
-#validate function takes input as exit status, and what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+source ./common.sh
+check root
 
 dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabling default nginx"
@@ -66,4 +35,4 @@ VALIDATE $? "copying nginx.conf"
 systemctl restart nginx 
 VALIDATE $? "restaring nginx"
 
-
+print_time
